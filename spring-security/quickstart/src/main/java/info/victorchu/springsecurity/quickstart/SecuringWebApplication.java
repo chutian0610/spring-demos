7dae1e0dfd54cff3282bc2df0de298292b6d569f
chutian0610 <victorchu0610@outlook.com>
2022-06-14 15:49:20 +0800

    feat: add spring security demo

---
+++ spring-security/quickstart/src/main/java/info/victorchu/springsecurity/quickstart/SecuringWebApplication.java
@@ -0,0 +1,13 @@
+package info.victorchu.springsecurity.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebApplication {
+
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+
+}
